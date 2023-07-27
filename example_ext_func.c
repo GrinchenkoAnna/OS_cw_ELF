0541d82d8419554777b2fe6d5c5ddd355e52fcbe
GrinchenkoAnna <grinchenkoanna@inbox.ru>
2023-07-28 00:56:04 +0700

    Created ELF-files
    1. example
    2. example_no_pie
    3. example_static
    4. example_dynamic
    
    The ELF files that were created used the rest of the files in the repository

---
+++ example_ext_func.c
@@ -0,0 +1,7 @@
+int hello_world_function();
+
+int main(int argc, char* argv[])
+{
+	int return_code = hello_world_function();
+	return return_code;
+}
---
+++ example_lib.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+extern int hello_world_function()
+{
+	printf("Hello, world.");
+	return 0;
+}
